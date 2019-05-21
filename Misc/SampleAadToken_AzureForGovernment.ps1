e61730e4bda7a32c992d4b940f1eaf92b34367bd
Steve Winward <stevewi@microsoft.com>
2019-05-21 17:49:56 -0400

    adding more debugging information

--- Misc/SampleAadToken_AzureForGovernment.ps1
+++ Misc/SampleAadToken_AzureForGovernment.ps1
@@ -16,7 +16,7 @@ $ClientSecret   = "<INSERT_ACTUAL_CLIENT_SECRET>" #This is the client secret you
 # This is the Azure for Government login URL
 $loginURL       = "https://login.microsoftonline.us"
 
-# This is the Grap API endpoint for Azure for Government
+# This is the Graph API endpoint for Azure for Government
 $resource       = "https://graph.microsoft.us" 
 
 # Constructs an HTTP POST request to get an OAuth token from AAD
@@ -24,7 +24,15 @@ $body       = @{grant_type="client_credentials";resource=$resource;client_id=$Cl
 $oauth      = Invoke-RestMethod -Method POST -Uri $loginURL/$tenantname/oauth2/token?api-version=1.0 -Body $body 
 
 # Outputs the JWT token for debugging
+Write-Output "JWT Token =>"
 $oauth.access_token
+Write-Output ""
+
+# Parses the JWT token for the role assignments to the AAD Application
+$payload = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($oauth.access_token.Split(".")[1]))
+Write-Output "Token Roles =>"
+($payload | ConvertFrom-Json).roles
+Write-Output ""
 
 # Sample call to list all users in the tenant
 # This requires application permissions to the User.ReadBasic.All (Graph API)
@@ -33,4 +41,5 @@ $headerParams = @{'Authorization'="$($oauth.token_type) $($oauth.access_token)"}
 $response = (Invoke-WebRequest -UseBasicParsing -Headers $headerParams -Uri $graphOperationUrl -Method GET) 
 
 # Outputs the Graph API response for debugging
+Write-Output "HTTP Response =>"
 $response
