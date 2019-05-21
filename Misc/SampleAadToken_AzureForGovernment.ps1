644f25e9bab342f07b2b163d75d6e6dfac22c38e
Steve Winward <stevewi@microsoft.com>
2019-05-20 22:55:12 -0400

    adding sample powershell script to create AAD access token

---
+++ Misc/SampleAadToken_AzureForGovernment.ps1
@@ -0,0 +1,11 @@
+﻿$tenantname     = "<INSERT_ACTUAL_TENANT_GUID>"
+$ClientID       = "<INSERT_ACTUAL_CLIENT_ID>" 
+$ClientSecret   = "<INSERT_ACTUAL_CLIENT_SECRET>"
+
+$loginURL       = "https://login.microsoftonline.us"  
+$resource       = "https://graph.microsoft.us" 
+
+$body       = @{grant_type="client_credentials";resource=$resource;client_id=$ClientID;client_secret=$ClientSecret} 
+$oauth      = Invoke-RestMethod -Method POST -Uri $loginURL/$tenantname/oauth2/token?api-version=1.0 -Body $body 
+
+$oauth.access_token
