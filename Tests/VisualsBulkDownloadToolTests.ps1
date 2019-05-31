dec1b894ae7f062da879be71211d6a715ea4437e
Steve Winward <stevewi@microsoft.com>
2019-05-31 09:40:59 -0400

    updating yaml file

---
+++ Tests/VisualsBulkDownloadToolTests.ps1
@@ -0,0 +1,14 @@
+Get-ChildItem
+
+Get-ChildItem -Path .\Misc\downloads -Filter *.pbiviz -Recurse      
+
+$pbiVizFileCount = (Get-ChildItem -Path .\Misc\downloads -Filter *.pbiviz -Recurse).Count
+$pbiVizFileCount
+
+$pbiVizFilesExist = $pbiVizFileCount -gt 0
+$pbiVizFilesExist
+
+Write-Output "Found $pbiVizFileCount pbiviz files in the downloads folder"
+
+# if the folder does not contain a pbiviz file, throw an exception
+if(-not $pbiVizFilesExist){ throw "No pbiviz files were downloaded" }
