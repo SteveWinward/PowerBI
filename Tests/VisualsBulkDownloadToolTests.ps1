4648d3b564659879d7a6c5516630b03a3b4b9e56
Steve Winward <stevewi@microsoft.com>
2019-05-31 09:47:53 -0400

    updating unit test

--- Tests/VisualsBulkDownloadToolTests.ps1
+++ Tests/VisualsBulkDownloadToolTests.ps1
@@ -3,12 +3,10 @@ Get-ChildItem
 Get-ChildItem -Path .\downloads -Filter *.pbiviz -Recurse      
 
 $pbiVizFileCount = (Get-ChildItem -Path .\downloads -Filter *.pbiviz -Recurse).Count
-$pbiVizFileCount
-
-$pbiVizFilesExist = $pbiVizFileCount -gt 0
-$pbiVizFilesExist
 
 Write-Output "Found $pbiVizFileCount pbiviz files in the downloads folder"
+
+$pbiVizFilesExist = $pbiVizFileCount -gt 0
 
 # if the folder does not contain a pbiviz file, throw an exception
 if(-not $pbiVizFilesExist){ throw "No pbiviz files were downloaded" }
