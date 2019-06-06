005fddf9968c6386040ba61be9bdf6e9f6892a16
Steve Winward <stevewi@microsoft.com>
2019-06-06 10:31:58 -0400

    updating yaml

--- Tests/Invoke-PesterAzureDevOps.ps1
+++ Tests/Invoke-PesterAzureDevOps.ps1
@@ -2,6 +2,10 @@
 # To run locally, simply just run Invoke-Pester, no need to run this script
 # https://github.com/devopsdina/ado-pipelines-demo-win/blob/master/Invoke-PesterAzureDevOps.ps1
 
+# Install Pester
+Install-Module -Name Pester -Force -SkipPublisherCheck
+
+# Import the Pester Module
 Import-Module Pester
 
 $outputFile = ".\TEST-RESULTS.xml"
