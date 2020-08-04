472b8018c802892b91ca91ec1d630940b6f51ddf
Steve Winward <2002602+SteveWinward@users.noreply.github.com>
2020-08-04 12:45:52 -0400

    adding teams metrics powershell scrip

---
+++ WriteUps/Scripts/Get-TeamsStats.ps1
@@ -0,0 +1,48 @@
+﻿Param
+(
+    [parameter(Mandatory=$true)]
+    [ValidateSet('Public','GCC','GCCHIGH','DOD')]
+    [string]
+    $EnvironmentName,
+
+    [parameter(Mandatory=$true)]
+    [string]
+    $OutputCsvFilePath
+)
+
+if($EnvironmentName -like 'Public'){
+    Connect-MicrosoftTeams
+}
+elseif($EnvironmentName -like 'GCC'){
+    Connect-MicrosoftTeams
+}
+elseif($EnvironmentName -like 'GCCHIGH'){
+    Connect-MicrosoftTeams -TeamsEnvironmentName 'TeamsGCCH'
+}
+elseif($EnvironmentName -like 'DOD'){
+    Connect-MicrosoftTeams -TeamsEnvironmentName 'TeamsDOD'
+}
+
+$hash = @{}
+
+$allTeams = Get-Team
+
+foreach($team in $allTeams){
+    $channels = Get-TeamChannel -GroupId $team.GroupId
+
+    foreach($channel in $channels){
+        $value = [PSCustomObject]@{
+                GroupId            = $team.GroupId
+                TeamName           = $team.DisplayName
+                ChannelId          = $channel.Id
+                ChannelName        = $channel.DisplayName
+                ChannelDescription = $channel.Description
+                Visibility         = $team.Visibility
+                Archived           = $team.Archived
+            }
+
+            $hash.Add($channel.Id, $value)
+    }
+}
+
+$hash.Values | Export-Csv -Path $OutputCsvFilePath -NoTypeInformation
